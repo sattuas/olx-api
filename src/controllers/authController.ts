bc536343b8470355c3e610cb7ecc62623841a66d
Raphael Melo <rafael.dks10@gmail.com>
2023-03-07 21:32:55 -0300

    registering

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,5 +1,9 @@
+import mongoose from 'mongoose';
 import { Request, Response } from 'express';
 import { validationResult, matchedData } from 'express-validator';
+import User from '../models/User';
+import State from '../models/State';
+import bcrypt from 'bcrypt';
 
 
 export const signIn = async (req: Request, res: Response) => {
@@ -17,8 +21,52 @@ export const signUp = async (req: Request, res: Response) => {
 
     const data = matchedData(req);
 
-    return res.json({
-        alright: true,
-        data: data
+    // checking if email already exists
+    const user = await User.findOne({
+        email: data.email
+    })
+    if(user) {
+        return res.json({
+            error: {
+                email: { msg: "Email already exists" }
+            }
+        })
+    }
+
+    // checking if state exists
+    if(mongoose.Types.ObjectId.isValid(data.state)) {
+        const stateItem = await State.findById(data.state);
+        if(!stateItem) {
+            return res.json({
+                error: {
+                    email: { state: "State does not exist" }
+                }
+            })
+        }
+    } else {
+        return res.json({
+            error: {
+                email: { state: "Invalid state code" }
+            }
+        })
+    }
+
+    const passwordHash = await bcrypt.hash(data.password, 10);
+
+    const payload = (Date.now() + Math.random()).toString();
+    const token = await bcrypt.hash(payload, 10);
+
+    
+    const newuser = new User({
+        name: data.name,
+        email: data.email,
+        passwordHash: passwordHash,
+        token: token,
+        state: data.state
     });
+
+    await newuser.save();
+    
+
+    return res.json({token});
 }
